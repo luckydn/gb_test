e9a76da9a5b51c1e9efe9b780cefd254fde4c066
luckydn <sp.donbass@gmail.com>
2022-08-10 11:53:29 +0300

    ignore

--- test/Program.cs
+++ test/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите 5 слов, либо чисел. Что бы программа отобразила все слова и числа которые имеют до 3х символов");
+int lengthArray = Convert.ToInt32(Console.ReadLine());
+
+string[] arraySymbol(int length)
+{
+    string[] result = new string[length];
+    for (int i = 0; i < length; i++)
+    {   
+        Console.WriteLine($"Введите {i+1} элемент");
+        result[i] = Convert.ToString(Console.ReadLine());
+    }
+    return result;
+}
